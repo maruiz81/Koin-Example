acbcf48d3c5991949bbcfd58d52a25306bca8a8e
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2019-08-17 16:14:41 +0200

    update libraries

--- app/src/main/java/com/maruiz/koinexample/presentation/application/KoinExampleApp.kt
+++ app/src/main/java/com/maruiz/koinexample/presentation/application/KoinExampleApp.kt
@@ -2,12 +2,19 @@ package com.maruiz.koinexample.presentation.application
 
 import android.app.Application
 import com.maruiz.koinexample.presentation.di.appModule
-import org.koin.android.ext.android.startKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class KoinExampleApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf(appModule), loadPropertiesFromFile = true)
+        startKoin {
+            androidContext(this@KoinExampleApp)
+            // modules
+            modules(appModule)
+
+            fileProperties()
+        }
     }
 }
--- app/src/main/java/com/maruiz/koinexample/presentation/di/modules.kt
+++ app/src/main/java/com/maruiz/koinexample/presentation/di/modules.kt
@@ -4,17 +4,17 @@ import com.maruiz.koinexample.data.api.BookApi
 import com.maruiz.koinexample.domain.usecase.GetBooks
 import com.maruiz.koinexample.presentation.adapter.BooksAdapter
 import com.maruiz.koinexample.presentation.viewmodel.BooksViewModel
-import org.koin.android.viewmodel.ext.koin.viewModel
-import org.koin.dsl.module.module
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
     single {
         Retrofit.Builder()
-                .baseUrl(getProperty<String>("base_url"))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            .baseUrl(getProperty<String>("base_url"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
     single { get<Retrofit>().create(BookApi::class.java) }
     single { GetBooks(get()) }
